f2c6a0d9f3c6eff5d865c27536d441051ad9ca87
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2022-10-28 10:51:49 +0800

    dev(deno): provided support for deno (#19)

--- dist/parser/data-structure/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './object.ts';
-export * from './array.ts';
--- dist/parser/data-structure/util.ts
+++
@@ -1,6 +0,0 @@
-import { ParameterlessLazyLoad } from '../../types/index.ts';
-
-const freeze = <T>(t: ParameterlessLazyLoad<T>, isImmutable: boolean) =>
-    !isImmutable ? t() : Object.freeze(t());
-
-export { freeze };
