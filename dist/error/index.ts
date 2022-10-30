15ec180783ce0c554cb0b2b43d2b5a0301e651be
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2022-10-30 16:33:03 +0800

    package(denoify): removed because it has no dependencies (#21)

--- dist/error/index.ts
+++
@@ -1,7 +0,0 @@
-export default class ParseError extends Error {
-    private constructor(message: string) {
-        super(message);
-    }
-
-    static fromMessage = (message: string) => new this(message);
-}
--- dist/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './parser/class/index.ts';
-export * from './parser/function/index.ts';
