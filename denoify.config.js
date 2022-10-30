67940bf7a07b419b0021de11bb233470a52c08bd
GervinFung <gervinfungdaxuen@gmail.com>
2022-10-30 16:23:43 +0800

    package(denoify): removed because it has no dependencies

--- denoify.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    index: 'src/index.ts',
-    out: 'dist',
-};
--- dist/parser/class/abstract.ts
+++
@@ -1,6 +0,0 @@
-export default abstract class Parser<T> {
-    protected constructor(protected readonly value: any) {}
-    abstract elseGet<A>(a: A): A | T;
-    abstract elseLazyGet<A>(a: () => A): A | T;
-    abstract elseThrow(message: string): T;
-}
